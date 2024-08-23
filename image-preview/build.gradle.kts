0e7449888589fdaf93d39ef4355b228852cd8ee0
MaksRoxx <maksimusroxx@gmail.com>
2024-08-23 19:54:39 +0300

    1.1

--- image-preview/build.gradle.kts
+++ image-preview/build.gradle.kts
@@ -56,7 +56,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.github.maksroxx"
                 artifactId = "image-preview-compose"
-                version = "1.0"
+                version = "1.1"
 
                 afterEvaluate{
                     from(components["release"])
